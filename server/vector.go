a4074b1252e0f7339b7648f58f09e76d033a378b
cdrpl <cdrpl@gmail.com>
2021-03-01 09:23:09 +0800

    server: refactor vector.go

--- server/vector.go
+++ server/vector.go
@@ -10,26 +10,6 @@ type Vector struct {
 	Y float64 `json:"y"`
 }
 
-// Return the distance from the given vector.
-func (v Vector) distance(v2 Vector) float64 {
-	x := math.Pow(v.X-v2.X, 2)
-	y := math.Pow(v.Y-v2.Y, 2)
-	dist := math.Sqrt(x + y)
-	return math.Abs(dist)
-}
-
-// Add the given vector to the current vector.
-func (v *Vector) add(vB Vector) {
-	v.X += vB.X
-	v.Y += vB.Y
-}
-
-// Multiply the vector by the given value.
-func (v *Vector) mult(val float64) {
-	v.X *= val
-	v.Y *= val
-}
-
 // Return the magnitude of the vector.
 func (v Vector) magnitude() float64 {
 	x := v.X * v.X
@@ -37,22 +17,48 @@ func (v Vector) magnitude() float64 {
 	return math.Sqrt(x + y)
 }
 
-// Return a directional vector from pos to dest.
-func direction(pos, dest Vector) (v Vector) {
-	v.X = dest.X - pos.X
-	v.Y = dest.Y - pos.Y
-	return
-}
-
 // Return the normalized vector
-func normalize(v Vector) Vector {
+func (v Vector) normalize() Vector {
 	mag := v.magnitude()
 
 	if mag == 0 {
 		return Vector{} // zeroed out vector
 	}
 
-	v.X /= mag
-	v.Y /= mag
-	return v
+	return Vector{
+		X: v.X / mag,
+		Y: v.Y / mag,
+	}
+}
+
+// Add the two vectors together.
+func vectorAdd(v1, v2 Vector) Vector {
+	return Vector{
+		X: v1.X + v2.X,
+		Y: v1.Y + v2.Y,
+	}
+}
+
+// Multiply the vector by the given value.
+func vectorMult(v Vector, val float64) Vector {
+	return Vector{
+		X: v.X * val,
+		Y: v.Y * val,
+	}
+}
+
+// Return the distance from the given vector.
+func vectorDist(v1, v2 Vector) float64 {
+	x := math.Pow(v1.X-v2.X, 2)
+	y := math.Pow(v1.Y-v2.Y, 2)
+	dist := math.Sqrt(x + y)
+	return math.Abs(dist)
+}
+
+// Return a directional vector from pos to dest.
+func vectorDir(src, dest Vector) Vector {
+	return Vector{
+		X: dest.X - src.X,
+		Y: dest.Y - src.Y,
+	}
 }
