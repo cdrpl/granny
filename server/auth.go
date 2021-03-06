45f51170d3c9b530c1c8f88e76cc2b4714e76788
cdrpl <cdrpl@gmail.com>
2021-03-07 00:59:43 +0800

    server: remove dead code

--- server/auth.go
+++ server/auth.go
@@ -4,25 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"strconv"
-	"strings"
 
 	"github.com/go-redis/redis/v8"
 )
-
-// Extracts the user ID and auth token from an authorization header.
-func parseAuthorization(authorization string) (id int64, token string, err error) {
-	split := strings.Split(authorization, ":")
-
-	if len(split) != 2 {
-		return 0, "", errors.New("Invalid authorization header")
-	}
-
-	id, err = strconv.ParseInt(split[0], 10, 4)
-	token = split[1]
-
-	return
-}
 
 // checkAuth will return true if the user is authorized and false if not.
 // Authorization is determined by checking redis for the user id and token.
